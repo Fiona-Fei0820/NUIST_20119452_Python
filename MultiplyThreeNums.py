544458bc723fc510d8802fcc38558c5742c81d9d
Fiona-Fei0820 <202583890007@nuist.edu.cn>
2026-03-23 19:11:08 +0800

    Add MultiplyThreeNums.py

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,9 @@
+def multiply(a,b,c):
+ product = float(a) * float(b) * float(c)
+ return product
+a = input("First number:")
+b = input("Second number:")
+c = input("Third number:")
+pro = multiply(a,b,c)
+print("The Answer is:")
+print(pro)
